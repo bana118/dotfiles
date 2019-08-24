cb11892230d307ce72df01619ede7a811e081e07
bana <bana.titech@gmail.com>
2019-08-24 23:58:15 +0900

    remove command in init_command.sh

--- wsl/init_command.sh
+++ wsl/init_command.sh
@@ -3,7 +3,6 @@ sed -i -e 's%http://.*.ubuntu.com%http://ftp.jaist.ac.jp/pub/Linux%g' /etc/apt/s
 apt update
 apt upgrade -y
 apt install -y vim-nox git php php-zip php-xml php-mbstring php-mysql apache2 libapache2-mod-php composer gcc make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev code
-git clone https://github.com/bana118/dotfiles.git ~
 sh ~/.dotfiles/wsl/link.sh
 source ~/.bash_profile
 source ~/.bashrc
