789ad5d32fc1e31642ca4acf4e77d7dc0b0e5f74
bana <bana.titech@gmail.com>
2019-09-15 12:02:15 +0900

    add commands for bitwarden

---
+++ aws_bitwarden/init_command.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo yum update
+sudo yum install -y docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose #see https://docs.docker.com/compose/install/
+sudo chmod +x /usr/local/bin/docker-compose
+curl -Lso bitwarden.sh https://go.btwrdn.co/bw-sh && chmod +x bitwarden.sh
