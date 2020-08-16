6b70f62ba21b944b280d1c2dfe1a6806030023b4
bana <bana.titech@gmail.com>
2020-08-16 11:43:15 +0900

    modify link.sh for devcontainer

--- devcontainer/link.sh
+++ devcontainer/link.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 ln -sf ~/dotfiles/devcontainer/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/devcontainer/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/devcontainer/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/devcontainer/.gitconfig ~/.gitconfig
