d9f64bb7492798f327db74ff71c9f7ec22103c15
bana118 <bana.titech@gmail.com>
2020-10-16 14:38:45 +0900

    use bashrc instead of bash_profile

--- devcontainer/link.sh
+++ devcontainer/link.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-ln -sf ~/dotfiles/devcontainer/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/devcontainer/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/devcontainer/git-completion.bash ~/git-completion.bash
+ln -sf ~/dotfiles/wsl/.alias ~/.alias
--- wsl/link.sh
+++ wsl/link.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 ln -sf ~/dotfiles/wsl/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/wsl/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/wsl/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/wsl/.alias ~/.alias
 ln -sf ~/dotfiles/wsl/.gitconfig ~/.gitconfig
