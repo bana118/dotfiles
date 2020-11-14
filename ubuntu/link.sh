353d885db32aa771d044e7a0adcd14f1d1fad05c
bana118 <bana.titech@gmail.com>
2020-11-15 04:40:44 +0900

    delete dotfiles for ubuntu rename dotfiles for bitwarden

--- ubuntu/link.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-ln -sf ~/dotfiles/ubuntu/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/ubuntu/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/ubuntu/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/ubuntu/.alias ~/.alias
-ln -sf ~/dotfiles/ubuntu/.gitconfig ~/.gitconfig
